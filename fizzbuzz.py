e6a322ba35af9665258f7fa5b8236cbd772fd1b1
Randall Arms <contact.randallArms@gmail.com>
2019-12-23 06:13:36 -0800

    v1.1 | Added input for total iterations
    * Added user input to select the total number of iterations.
    * Added a check to make sure input does not cause unreasonably long processes.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,11 +1,22 @@
-for fizzbuzz in range(100):
-    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
-        print("FizzBuzz")
-        continue
-    elif fizzbuzz % 3 == 0:
-        print("Fizz")
-        continue
-    elif fizzbuzz % 5 == 0:
-        print("Buzz")
-        continue
-    print(fizzbuzz)
+print("Up to which number would you like to iterate through? ")
+i = input();
+
+if int(i) > 9999:
+    print("This set has more than ten thousand iterations. Type yes to confirm: ")
+    confirm = input();
+    if confirm != "yes":
+	    print("Iteration canceled. ")
+    else:
+        print("Iterating through each integer up to " + i + ": ")
+		
+        for fizzbuzz in range(int(i)):
+            if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+                print("FizzBuzz")
+                continue
+            elif fizzbuzz % 3 == 0:
+                print("Fizz")
+                continue
+            elif fizzbuzz % 5 == 0:
+                print("Buzz")
+                continue
+            print(fizzbuzz)
