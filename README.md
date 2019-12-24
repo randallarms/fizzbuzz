74dd295cd1abf501c034dcdcdf3981d257351199
Randall Arms <contact.randallArms@gmail.com>
2019-12-23 19:01:12 -0800

    v1.2 | Range is now inclusive, starts from 1
    * The range of the iterations is now from 1 to the input (inclusive), versus the previous range of 0 to the input (exclusive).

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # fizzbuzz
+*Python*
+
+## Summary
 Iterate through a set of integers and print "Fizz" if equally divisible by 3, "Buzz" if equally divisible by 5, or "FizzBuzz" if equally divisible by both 3 and 5.
+
+## Instructions
+When prompted, type in the total number of integer iterations desired and hit enter.
+> Note: If the number is five digits long or more (i.e., a number greater than 9999) then a confirmation will be requested. Type "yes" and hit enter to continue.
+
+The program should then iterate through each integer from 1 to the number selected, inclusive.
+> Note: More options will be added soon for starting from a selected integer.
+
+## About
+This project is a personal spin on the popular programming exercise based on the children's game of the same name. For now, the program runs in Python, but alternatives may be added in the future. 
--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,22 +1,21 @@
-print("Up to which number would you like to iterate through? ")
+print("Up to which number would you like to iterate? ")
 i = input();
 
 if int(i) > 9999:
-    print("This set has more than ten thousand iterations. Type yes to confirm: ")
+    print("This set has more than ten thousand iterations and may take a very long time to finish. Type yes to confirm: ")
     confirm = input();
     if confirm != "yes":
 	    print("Iteration canceled. ")
-    else:
-        print("Iterating through each integer up to " + i + ": ")
-		
-        for fizzbuzz in range(int(i)):
-            if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
-                print("FizzBuzz")
-                continue
-            elif fizzbuzz % 3 == 0:
-                print("Fizz")
-                continue
-            elif fizzbuzz % 5 == 0:
-                print("Buzz")
-                continue
-            print(fizzbuzz)
+	    exit()
+
+print("Iterating through each integer up to " + i + ": ")
+
+for fizzbuzz in range(1, int(i)+1):
+	output = ""
+
+	if fizzbuzz % 3 == 0:
+		output += "Fizz"
+	if fizzbuzz % 5 == 0:
+		output += "Buzz"
+
+	print(output or fizzbuzz)
