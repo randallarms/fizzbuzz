8b9b6f9bd8a5ab29cace63b21c9beff5ef4d3258
Randall Arms <contact.randallArms@gmail.com>
2019-12-29 03:51:50 -0800

    v2.0 | Updated code & inputs
    * Can now input the individual divisor integers, replacing the default '3' and '5'.
    * Only integers are accepted input now.
    * Minor text improvements.

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # fizzbuzz
 Written in *Python*
+by *Randall Arms* @ randallarms.com
 
 ## Summary
 Iterate through a set of integers and print "Fizz" if equally divisible by 3, "Buzz" if equally divisible by 5, or "FizzBuzz" if equally divisible by both 3 and 5.
 
 ## Instructions
 When prompted, type in the total number of integer iterations desired and hit enter.
-> Note: If the number has more than three digits (i.e., any number greater than 999) then a confirmation will be requested. Type "yes" and hit enter to continue.
+> Note: If the number has more than a thousand then a confirmation will be requested. Type "yes" and hit enter to continue.
+
+You will then be prompted first for the "Fizz" divisor and then for the "Buzz" divisor. To get the traditional results, enter "3" and then "5"; however, any positive integer can be used in their place.
 
 The program should then iterate through each integer from 1 to the number selected, inclusive.
 > Note: More options will be added soon for starting from a selected integer.
@@ -14,4 +17,4 @@ The program should then iterate through each integer from 1 to the number select
 ## About
 This project is a personal spin on the popular programming exercise based on the children's game of the same name. For now, the program runs in Python, but alternatives may be added in the future.
 
-A minimal version of the code can be found in the file "fizzbuzz-short.py", which generates the traditional FizzBuzz set from 1 to 100 in as few characters as possible.
+A minimal version of the code can be found in the file "fizzbuzz-short.py", which generates the traditional FizzBuzz set from 1 to 100 in as few code characters as possible. Currently, the fizzbuzz-short.py contains fewer than 150 characters.
--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,19 +1,51 @@
-print("=========")
+#=====================================#
+#             FizzBuzz                #
+#          by Randall Arms            #
+#  @ github.com/randallarms/fizzbuzz  #
+#=====================================#
+
+# Opening text
+print("\n\n=========")
 print("FIZZBUZZ!")
 print("=========")
 
-print("Up to which number would you like to iterate? ")
+# Checks that the input n is a positive integer and returns an obj of int type
+def int_check(n):
+    try:
+        return int(n)
+    except ValueError:
+        print("\nInput number must be a positive integer. ")
+        exit()
+
+# Input the maximum integer to iterate to
+print("\nUp to which number would you like to iterate? ")
 i = input("> ");
 
-if int(i) > 999:
-    print("Set is greater than 1000 and may take a long time to finish. Type yes to confirm: ")
+i_int = int_check(i)
+
+# Check for large numbers and confirm
+if int(i) > 1000:
+    print("\nSet is greater than 1000 and may take a long time to finish. Type yes to confirm: ")
     confirm = input("> ");
     if confirm != "yes":
-	    print("Iteration canceled. ")
+	    print("\nIteration canceled. ")
 	    exit()
 
-print("Iterating through each integer up to " + i + ": ")
+# Input "Fizz" divisor
+print("\nWhich number would you like to check divisibility by for printing 'Fizz'? ")
+f = input("> ");
+
+f_int = int_check(f)
+
+# Input "Buzz" divisor
+print("\nWhich number would you like to check divisibility by for printing 'Buzz'? ")
+b = input("> ");
+
+b_int = int_check(b)
+
+# Iterate through the integers and print results
+print("\nIterating through each integer up to " + i + ": ")
 for num in range(1, int(i)+1):
-	fizz = (num % 3 == 0)
-	buzz = (num % 5 == 0)
+	fizz = (num % f_int == 0)
+	buzz = (num % b_int == 0)
 	print('FizzBuzz' if (fizz & buzz) else 'Fizz' if fizz else 'Buzz' if buzz else num)
